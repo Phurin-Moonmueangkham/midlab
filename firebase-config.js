cb9cf00dbe0153b9efb1569abec5ea1b1ed6f0c7
Phurin-Moonmueangkham <6731503026@lamduan.mfu.ac.th>
2026-03-13 09:37:27 +0000

    done

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Replace placeholder values with your real Firebase Web App config from Firebase Console.
 export const firebaseConfig = {
-  apiKey: "REPLACE_WITH_API_KEY",
+  apiKey: "AIzaSyCQ2KB1G6jGlTb9Zab0b6w87GqE0A0SqIs",
   authDomain: "midturm-6adfd.firebaseapp.com",
   projectId: "midturm-6adfd",
   storageBucket: "midturm-6adfd.firebasestorage.app",
-  messagingSenderId: "REPLACE_WITH_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_APP_ID"
+  messagingSenderId: "944628496103",
+  appId: "1:944628496103:web:fb2109ea5c43820a5c1442"
 };
